a7bdd2688f999f19bf300d6f6a8e3b5c309dce2e
IvanL <ilychev@no.email>
2021-05-02 14:47:23 +0300

    Init

---
+++ hh-full-structs.go
@@ -0,0 +1,169 @@
+
+// The struct is generated from HH's response using https://mholt.github.io/json-to-go/
+type Vacancy struct {
+	Details                *VacancyDetails // custom field, not from HH respond
+	ID                     string      `json:"id"`
+	Premium                bool        `json:"premium"`
+	Name                   string      `json:"name"`
+	Department             interface{} `json:"department"`
+	HasTest                bool        `json:"has_test"`
+	ResponseLetterRequired bool        `json:"response_letter_required"`
+	Area                   struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"area"`
+	Salary interface{} `json:"salary"`
+	Type   struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"type"`
+	Address           interface{}   `json:"address"`
+	ResponseURL       interface{}   `json:"response_url"`
+	SortPointDistance interface{}   `json:"sort_point_distance"`
+	PublishedAt       string        `json:"published_at"`
+	CreatedAt         string        `json:"created_at"`
+	Archived          bool          `json:"archived"`
+	ApplyAlternateURL string        `json:"apply_alternate_url"`
+	InsiderInterview  interface{}   `json:"insider_interview"`
+	URL               string        `json:"url"`
+	AlternateURL      string        `json:"alternate_url"`
+	Relations         []interface{} `json:"relations"`
+	Employer          struct {
+		ID           string `json:"id"`
+		Name         string `json:"name"`
+		URL          string `json:"url"`
+		AlternateURL string `json:"alternate_url"`
+		LogoUrls     struct {
+			Num90    string `json:"90"`
+			Num240   string `json:"240"`
+			Original string `json:"original"`
+		} `json:"logo_urls"`
+		VacanciesURL string `json:"vacancies_url"`
+		Trusted      bool   `json:"trusted"`
+	} `json:"employer"`
+	Snippet struct {
+		Requirement    string `json:"requirement"`
+		Responsibility string `json:"responsibility"`
+	} `json:"snippet"`
+	Contacts interface{} `json:"contacts"`
+	Schedule struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"schedule"`
+	WorkingDays          []interface{} `json:"working_days"`
+	WorkingTimeIntervals []interface{} `json:"working_time_intervals"`
+	WorkingTimeModes     []interface{} `json:"working_time_modes"`
+	AcceptTemporary      bool          `json:"accept_temporary"`
+} 
+
+type Vacancies struct {
+	Items []Vacancy `json:"items"`
+	// The following fields are in the HH's response but we dont use them
+	// Found        int         `json:"found"`
+	// Pages        int         `json:"pages"`
+	// PerPage      int         `json:"per_page"`
+	// Page         int         `json:"page"`
+	// Clusters     interface{} `json:"clusters"`
+	// Arguments    interface{} `json:"arguments"`
+	// AlternateURL string      `json:"alternate_url"`
+}
+
+type VacancyDetails struct {
+	ID          string `json:"id"`
+	Premium     bool   `json:"premium"`
+	BillingType struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"billing_type"`
+	Relations              []interface{} `json:"relations"`
+	Name                   string        `json:"name"`
+	InsiderInterview       interface{}   `json:"insider_interview"`
+	ResponseLetterRequired bool          `json:"response_letter_required"`
+	Area                   struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"area"`
+	Salary struct {
+		From     int    `json:"from"`
+		To       int    `json:"to"`
+		Currency string `json:"currency"`
+		Gross    bool   `json:"gross"`
+	} `json:"salary"`
+	Type struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"type"`
+	Address       interface{} `json:"address"`
+	AllowMessages bool        `json:"allow_messages"`
+	Site          struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"site"`
+	Experience struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"experience"`
+	Schedule struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"schedule"`
+	Employment struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"employment"`
+	Department                 interface{} `json:"department"`
+	Contacts                   interface{} `json:"contacts"`
+	Description                string      `json:"description"`
+	BrandedDescription         interface{} `json:"branded_description"`
+	VacancyConstructorTemplate interface{} `json:"vacancy_constructor_template"`
+	KeySkills                  []struct {
+		Name string `json:"name"`
+	} `json:"key_skills"`
+	AcceptHandicapped bool        `json:"accept_handicapped"`
+	AcceptKids        bool        `json:"accept_kids"`
+	Archived          bool        `json:"archived"`
+	ResponseURL       interface{} `json:"response_url"`
+	Specializations   []struct {
+		ID           string `json:"id"`
+		Name         string `json:"name"`
+		ProfareaID   string `json:"profarea_id"`
+		ProfareaName string `json:"profarea_name"`
+	} `json:"specializations"`
+	Code                    interface{}   `json:"code"`
+	Hidden                  bool          `json:"hidden"`
+	QuickResponsesAllowed   bool          `json:"quick_responses_allowed"`
+	DriverLicenseTypes      []interface{} `json:"driver_license_types"`
+	AcceptIncompleteResumes bool          `json:"accept_incomplete_resumes"`
+	Employer                struct {
+		ID           string `json:"id"`
+		Name         string `json:"name"`
+		URL          string `json:"url"`
+		AlternateURL string `json:"alternate_url"`
+		LogoUrls     struct {
+			Num90    string `json:"90"`
+			Num240   string `json:"240"`
+			Original string `json:"original"`
+		} `json:"logo_urls"`
+		VacanciesURL string `json:"vacancies_url"`
+		Trusted      bool   `json:"trusted"`
+	} `json:"employer"`
+	PublishedAt          string        `json:"published_at"`
+	CreatedAt            string        `json:"created_at"`
+	NegotiationsURL      interface{}   `json:"negotiations_url"`
+	SuitableResumesURL   interface{}   `json:"suitable_resumes_url"`
+	ApplyAlternateURL    string        `json:"apply_alternate_url"`
+	HasTest              bool          `json:"has_test"`
+	Test                 interface{}   `json:"test"`
+	AlternateURL         string        `json:"alternate_url"`
+	WorkingDays          []interface{} `json:"working_days"`
+	WorkingTimeIntervals []interface{} `json:"working_time_intervals"`
+	WorkingTimeModes     []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"working_time_modes"`
+	AcceptTemporary bool `json:"accept_temporary"`
+}
+
+/////////////////
---
+++ hh.go
@@ -0,0 +1,307 @@
+package main
+
+import (
+	"os"
+	"sync"
+	"regexp"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"encoding/json"
+	"github.com/grokify/html-strip-tags-go"
+	//"strings"
+	"text/template"
+	"crypto/md5"
+	//"errors"
+)
+
+const HH_SEARCH_KEYWORD = "devops"
+const VACANCIES_MAX_TOTAL = 1999 // HH.RU API allows up to 2000 vacancies only
+const VACANCIES_PER_PAGE = 99    // ...again, this is upper limit posed by HH.RU
+//const PAGES_COUNT = 2            
+var PAGES_COUNT = VACANCIES_MAX_TOTAL / VACANCIES_PER_PAGE
+
+const numDownloaders = 20
+
+const VACANCIES_AGE = 30
+var VACANCIES_PAGE_BASE_URL = fmt.Sprintf(
+	"https://api.hh.ru/vacancies?text=%s" +
+	"&per_page=%d" +
+	"&period=%d" +
+	"&vacancy_search_order=publication_date&page=",
+	HH_SEARCH_KEYWORD,
+	VACANCIES_PER_PAGE,
+	VACANCIES_AGE)
+
+const API_VACANCIES_BASE_URL = "https://api.hh.ru/vacancies/"
+const VACANCY_BASE_URL = "https://hh.ru/vacancy/"
+
+// The struct is generated from HH's response using https://mholt.github.io/json-to-go/
+// Some fields are not used and thus removed
+type Vacancy struct {
+	Schedule struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"schedule"`
+	Details                *VacancyDetails // custom field, not from HH respond
+	ID                     string      `json:"id"`
+	Name                   string      `json:"name"`
+	Area                   struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"area"`
+	AdditionalAreas        []string // if we find the vacancy in other regions, add them in here
+	NormalizedSalary       string // convert any salary to a common value ($)
+	Salary interface{} `json:"salary"`
+	Type   struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"type"`
+	Address           interface{}   `json:"address"`
+	ResponseURL       interface{}   `json:"response_url"`
+	SortPointDistance interface{}   `json:"sort_point_distance"`
+	PublishedAt       string        `json:"published_at"`
+	CreatedAt         string        `json:"created_at"`
+	Archived          bool          `json:"archived"`
+	Employer          struct {
+		ID           string `json:"id"`
+		Name         string `json:"name"`
+		URL          string `json:"url"`
+		Trusted      bool   `json:"trusted"`
+	} `json:"employer"`
+	Snippet struct {
+		Requirement    string `json:"requirement"`
+		Responsibility string `json:"responsibility"`
+	} `json:"snippet"`
+	Contacts interface{} `json:"contacts"`
+	AcceptTemporary      bool          `json:"accept_temporary"`
+} 
+
+type Vacancies struct {
+	Items []Vacancy `json:"items"`
+}
+
+type VacancyDetails struct {
+	ID          string `json:"id"`
+	Name                   string        `json:"name"`
+	Description                string      `json:"description"`
+	Excerpt                string // HTMLed excerpt of the description where keyword found
+	Hash                   string // md5 of the description (with no HTML tags)
+}
+
+/////////////////
+
+func pf(s string) {
+	fmt.Printf("[Print] %s\n", s)
+}
+
+func getAllVacanciesList(done <-chan struct{}) (chan Vacancy) {
+	allVacancies := make(chan Vacancy)
+	go func() {
+		defer close(allVacancies)
+		for page := 1; page <= PAGES_COUNT; page++ {
+			url := fmt.Sprintf("%s" + "%d", VACANCIES_PAGE_BASE_URL, page)
+			resp, err := http.Get(url)
+			if err != nil {
+				pf(fmt.Sprintf("Error %s for %s\n", err, url))
+				// log.Fatalln(err)
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+
+			vacancies := &Vacancies{}
+			err = json.Unmarshal(body, &vacancies)
+			if err != nil {
+				//log.Println(err)
+				fmt.Println(err)
+			}
+		
+			items := vacancies.Items
+			for i := 0; i < len(items); i++ {
+				//vac := &items[i]
+				select {
+					case allVacancies <- items[i]:
+						// pf(
+						// 	fmt.Sprintf(
+						// 		"page %d - getVacancies(): № %d '%s' in '%s'\n", page, i + 1, vac.Name, vac.Employer.Name))
+					case <-done:
+						return //nil //errors.New("walk canceled")
+				}
+				
+			}
+		}
+		return //nil
+	}()
+	return allVacancies
+}
+
+func getVacancyDescription(done <-chan struct{}, allVacancies chan Vacancy, withDescriptions chan<- Vacancy, myId int) {
+	//pf(fmt.Sprintf("start getVacancyDescription() № %d\n", myId))
+	for vac := range allVacancies {
+		url := API_VACANCIES_BASE_URL + vac.ID
+		//pf("Fetching " + url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("Error %s for %s\n", err, url)
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		details := &VacancyDetails{}
+		err = json.Unmarshal(body, &details)
+		if err != nil {
+			//log.Println(err)
+			fmt.Println(err)
+		}
+		plain := strip.StripTags(details.Description)
+		byted := []byte(plain)
+		details.Hash = fmt.Sprintf("%x", md5.Sum(byted))
+
+		//fmt.Printf("getVacancyDescription() № %d Vacancy: %s, descr: %s\n", myId, vac.Name, details.Description[:12])
+		vac.Details = details
+		select {
+		case withDescriptions <- vac:
+		case <-done:
+			{
+		    fmt.Printf("\t%d got done, EXIT\n", myId)
+			return
+		  }
+		}
+	}
+	//fmt.Printf("\t%d EXITing as allVacancies closed\n", myId)
+}
+/* 
+func normalizeSalary(vac Vacancy) string {
+	salary := ""
+	RurToDoll := 74
+	var from int
+	var to int
+	if vac.Salary == nil {
+		return salary
+	}
+	if vac.Salary == "USD" {
+		salary += "$ "
+	}
+	if vac.Salary.from != nil {
+		from = vac.Salary.from 
+	}
+	if vac.Salary.to != nil {
+		to = vac.Salary.from 
+	}
+	if vac.Salary == "RUR" {
+        to = to / RurToDoll
+		from = from / RurToDoll
+	}
+    return salary
+}
+ */
+/* 
+func getExcerpt(keyword, txt string) (string) {
+	txt = strip.StripTags(txt)
+	contextSize := 50
+	fullLen := len(txt)
+	pos := strings.Index(txt, keyword)
+	startPos := pos - contextSize
+	endPos := pos + len(keyword) + contextSize
+	if startPos < 0 { startPos = 0 }
+	if endPos > fullLen - 1 { endPos = fullLen - 1}
+	excerpt := fmt.Sprintf("...%s<b>%s</b>%s...",
+		txt[startPos:pos],
+		keyword,
+		txt[pos + len(keyword):endPos])
+	return excerpt
+}
+ */
+ func getExcerpt(txt string) (string) {
+	excerpt := ""
+	pattern := "удален|remote"
+	r, _ := regexp.Compile(pattern)
+	txt = strip.StripTags(txt)
+	pos := r.FindStringIndex(txt)
+	if pos == nil {
+		return excerpt
+	}
+	contextSize := 50
+	fullLen := len(txt)
+
+	startPos := pos[0] - contextSize
+	endPos := pos[1] + contextSize
+	if startPos < 0 { startPos = 0 }
+	if endPos > fullLen - 1 { endPos = fullLen - 1}
+	excerpt = fmt.Sprintf("...%s<b>%s</b>%s...",
+		txt[startPos:pos[0]],
+		txt[pos[0]:pos[1]],
+		txt[pos[1]:endPos])
+	return excerpt
+}
+
+
+func main() {
+	done := make(chan struct{})
+	defer close(done)
+
+	allVacancies := getAllVacanciesList(done)
+	
+	withDescription := make(chan Vacancy)
+
+	var downloadersWg sync.WaitGroup
+	downloadersWg.Add(numDownloaders)
+	downloaderId := 1
+	for d := 0; d < numDownloaders; d++ {
+		downloaderId++
+		go func() {
+            getVacancyDescription(done, allVacancies, withDescription, downloaderId)
+			downloadersWg.Done()
+		}()
+	}
+	go func() {
+		downloadersWg.Wait()
+		close(withDescription)
+	}()
+	i := 0
+	notRemote := 0
+	var remoteOnes []Vacancy 
+	type RemoteVacancies struct{
+		Title string
+		Items []Vacancy
+	}
+
+	seenBefore := map[string]bool{}
+	seenBeforeCount := 0
+
+	for vac := range withDescription {
+		i++
+		//pos = 0
+		   //pos := strings.Index(vac.Details.Description, "удален"); pos != 0 ||{
+		vac.Details.Excerpt = getExcerpt(vac.Details.Description)
+		if  vac.Schedule.ID == "remote" || len(vac.Details.Excerpt) > 0 {
+			pf(fmt.Sprintf("%d (%s) [sal: %v] '%s' in '%s'\n\t%s", i, VACANCY_BASE_URL + vac.ID, vac.Salary,vac.Name, vac.Employer.Name, vac.Details.Excerpt))
+			if vac.Salary == nil {
+				vac.NormalizedSalary  = ""
+			} else {
+				vac.NormalizedSalary = fmt.Sprintf("%v", vac.Salary)
+			}
+			_, seen := seenBefore[vac.Details.Hash]
+			if seen {
+				pf("Seen before")
+				seenBeforeCount++
+			} else {
+				seenBefore[vac.Details.Hash] = true
+				remoteOnes = append(remoteOnes, vac)
+			}
+		} else {
+			notRemote++
+		}
+	}
+	data := RemoteVacancies{Title: "vacancies", Items: remoteOnes}
+	//data.Items = remoteOnes
+	pf(fmt.Sprintf("Skipped not remote ones: %d", notRemote))
+	pf(fmt.Sprintf("Duplicated: %d", seenBeforeCount))
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	f, err := os.Create("index2.html")
+	if err != nil {
+		// handle error
+	}
+	tmpl.Execute(f, data)
+}
